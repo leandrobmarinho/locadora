858ecdb4ee0bf293ab77dca2392d1c4322c8120e
leandrobmarinho <leandrobezerramarinho@gmail.com>
2014-05-28 23:17:27 -0300

    Primeiro

---
+++ Locadora/src/com/livro/capitulo3/util/ConectaHibernateMySQL.java
@@ -0,0 +1,17 @@
+package com.livro.capitulo3.util;
+
+import org.hibernate.Session;
+
+public class ConectaHibernateMySQL {
+	
+	public static void main(String[] args) {
+		Session sessao = null;
+		
+		try{
+			sessao = HibernateUtil.getSessionFactory().openSession();
+			System.out.println("Conectou!");
+		}finally{
+			sessao.close();
+		}
+	}
+}
